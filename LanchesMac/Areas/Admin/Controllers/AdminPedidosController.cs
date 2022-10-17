6fd86b670771c746e401cc4b271726e337eb2245
Leonardo Souza Martins <LSMartins@santoandre.sp.gov.br>
2022-10-17 15:12:49 -0300

    Implementação dos detalhes COM ERRO

--- LanchesMac/Areas/Admin/Controllers/AdminPedidosController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminPedidosController.cs
@@ -10,6 +10,7 @@ using LanchesMac.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Data;
 using ReflectionIT.Mvc.Paging;
+using LanchesMac.ViewModels;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
@@ -22,6 +23,27 @@ namespace LanchesMac.Areas.Admin.Controllers
         public AdminPedidosController(AppDbContext context)
         {
             _context = context;
+        }
+
+        public IActionResult PedidoLanches(int? id)
+        {
+            var pedido = _context.Pedidos
+                .Include(pd => pd.PedidoItens)
+                .ThenInclude(l => l.Lanche)
+                .FirstOrDefault(p => p.PedidoId == id);
+
+            if (pedido == null)
+            {
+                Response.StatusCode = 404;
+                return View("PedidoNotFound", id.Value);
+            }
+
+            PedidoLancheViewModel pedidoLanches = new PedidoLancheViewModel()
+            {
+                Pedido = pedido,
+                PedidoDetalhes = pedido.PedidoItens
+            };
+            return View(pedidoLanches);
         }
 
         //não será necessário método action index pois view será paginada, não sendo necessária view index gerada
---
+++ LanchesMac/ViewModels/PedidoLancheViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewModels
+{
+    public class PedidoLancheViewModel
+    {
+        public Pedido Pedido { get; set; }
+        public IEnumerable<PedidoDetalhe> PedidoDetalhes { get; set; }
+    }
+}
